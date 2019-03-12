3395bd3afa92ef0ddbb5dc2265b2d1e24e659054
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2019-03-12 14:07:48 -0400

    Add files via upload

---
+++ Chapter1.py
@@ -0,0 +1,8 @@
+print('What is your favorite movie?') # ask for their favorite movie
+movie1 = input()
+print('What is your second favorite movie?') # ask for 2nd favorite movie
+movie2 = input()
+print('What is your third favorite movie?') #ask for 3rd favorite movie
+movie3 = input()
+print('The length of your movies are ') #determine the length of the 3 movies
+print(len(movie1 + movie2 + movie3))

843479b12a80995b67ac96b5c740d9794eac65d7
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2019-03-20 14:29:12 -0400

    Add files via upload

---
+++ Chapter2.py
@@ -0,0 +1,15 @@
+number = input()
+
+if number != '9':
+
+    if number == '1':
+        print('1, is the first number')
+        
+    if number == '2':
+        print('2, is the second number')
+
+    if number == '3':
+        print('3, is the third number')
+
+    if number == '4':
+        print('4, is the fourth number')

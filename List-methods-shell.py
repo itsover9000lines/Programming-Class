63848cf430af391e0970d6b98e5bf85c694eec0e
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2019-04-23 13:53:05 -0400

    Add files via upload

---
+++ List-methods-shell.py
@@ -0,0 +1,59 @@
+spam = ['hello', 'hi', 'howdy', 'heyas']
+spam.index('hello')
+0
+spam.index('heyas')
+3
+spam.index('howdy howdy howdy')
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+ValueError: 'howdy howdy howdy' is not in list
+spam = ['Zophie', 'Pooka', 'Fat-tail', 'Pooka']
+spam.index('Pooka')
+1
+spam = ['cat', 'dog', 'bat']
+spam.append('moose')
+spam
+['cat', 'dog', 'bat', 'moose']
+spam = ['cat', 'dog', 'bat']
+spam.insert(1, 'chicken')
+spam
+['cat', 'chicken', 'dog', 'bat']
+spam = ['cat', 'bat', 'rat', 'elephant']
+spam.remove('bat')
+spam
+['cat', 'rat', 'elephant']
+spam = ['cat', 'bat', 'rat', 'elephant']
+spam.remove('chicken')
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+ValueError: list.remove(x): x not in list
+spam = ['cat', 'bat', 'rat', 'cat', 'hat', 'cat']
+spam.remove('cat')
+spam
+['bat', 'rat', 'cat', 'hat', 'cat']
+spam = [2, 5, 3.14, 1, -7]
+spam.sort()
+spam
+[-7, 1, 2, 3.14, 5]
+spam = ['ants', 'cats', 'dogs', 'badgers', 'elephants']
+spam.sort()
+spam
+['ants', 'badgers', 'cats', 'dogs', 'elephants']
+spam.sort(reverse=True)
+spam
+['elephants', 'dogs', 'cats', 'badgers', 'ants']
+
+spam = [1, 3, 2, 4, 'Alice', 'Bob']
+spam.sort()
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: '<' not supported between instances of 'str' and 'int'
+spam = ['Alice', 'ants', 'Bob', 'badgers', 'Carol', 'cats']
+spam.sort()
+spam
+['Alice', 'Bob', 'Carol', 'ants', 'badgers', 'cats']
+spam = ['a', 'z', 'A', 'Z']
+spam.sort(key=str.lower)
+spam
+['a', 'A', 'z', 'Z']
+
---
+++ Looping-Lists-Shell.py
@@ -0,0 +1,22 @@
+for i in range(4):
+    print(i)
+
+0
+1
+2
+3
+for i in [0, 1, 2, 3]:
+    print(i)
+
+0
+1
+2
+3
+supplies = ['pens', 'staplers', 'flame-throwers', 'binders']
+for i in range(len(supplies)):
+    print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
+
+Index 0 in supplies is: pens
+Index 1 in supplies is: staplers
+Index 2 in supplies is: flame-throwers
+Index 3 in supplies is: binders

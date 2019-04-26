df4ad39a56f5e3d0b4928420064bbb94d4248631
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2019-04-26 10:24:20 -0400

    Add files via upload

--- Chapter4.py
+++ Chapter4.py
@@ -1,8 +1,13 @@
-import random
+import random # imports the random module
 
 avengers = ['Thor', 'Iron Man', 'Spiderman', 'Hulk', 'Falcon', 'Black Widow', 'Antman', 'Captain America']
 verb = ['smashed', 'kicked', 'slapped', 'bonked']
 villian = []
+# these are the lists that the script will use
 
 for i in range(8):
     villian.append(input('Enter 8 villian names. (Villian number ' + str(i + 1) + ')')) # asks user to list 8 villians
+
+print('Welcom to Avengers: End of Line!')
+input('Press "Enter" to continue.') # asks the user to press the enter key to continue the script
+

bb77d179ae18a433703f29df71941d3c4dab5efb
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2019-05-09 14:53:55 -0400

    Add files via upload

--- Chapter4.py
+++ Chapter4.py
@@ -16,6 +16,7 @@ the Avengers winning, or the villians.
 '''
 
 import random # imports the random module
+import secrets # imports the secrets module
 
 avengers = ['Thor', 'Iron Man', 'Spiderman', 'Hulk', 'Falcon', 'Black Widow', 'Antman', 'Captain America']
 verb = ['smashed', 'kicked', 'slapped', 'bonked']
@@ -28,11 +29,45 @@ for i in range(8):
 print('Welcom to Avengers: End of Line!') # welcomes the user
 input('Press "Enter" to continue.') # asks the user to press the enter key to continue the script
 
+'''
 selectedAvenger = random.choice(avengers) # selects a random avenger
 selectedVillian = random.choice(villian) # selects a random villian
 selectedVerb = random.choice(verb) # selects a random verb
 
-winner = random.choice([selectedAvenger, selectedVillian])
+winner = random.choice([selectedAvenger, selectedVillian]) # selects a winner
 
+if winner == selectedAvenger:
+    villian.remove(selectedVillian)
 
-print(winner)
+else: avengers.remove(selectedAvenger)
+
+# if the winner is the avengers, then the villian gets removed, and vice versa
+'''
+
+while avengers or villian > 2:
+
+    selectedAvenger = random.choice(avengers) # selects a random avenger
+    selectedVillian = random.choice(villian) # selects a random villian
+    selectedVerb = random.choice(verb) # selects a random verb
+
+    winner = [selectedAvenger, selectedVillian]
+
+    loser = random.choice(winner) # selects a loser
+   # print(winner)
+   # print(loser)
+    winner.remove(loser) # removes the loser from the winner list
+   # print(winner)
+
+    print(winner + ' ' + selectedVerb + ' ' + loser) # prints the result
+
+    if winner == selectedAvenger:
+        villian.remove(selectedVillian)
+
+    else: avengers.remove(selectedAvenger)
+# if the winner is the avengers, then the villian gets removed, and vice versa
+
+if avengers > 1:
+    print('The Avengers win! The surviving members are ' + avengers + '!')
+else:
+    print('The villians win! The surviving memebrs are ' + villian + '!')
+# prints the final result

847dad1dfc3d875b8ae766746886f34039050d0e
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2019-06-03 14:52:48 -0400

    Add files via upload

--- Chapter5.py
+++ Chapter5.py
@@ -8,24 +8,25 @@ End of Line
 
 5/23/19
 
-This program has a set of 8 Avengers, the user running the program is asked to enter 8 villian
-names, and power levels. The program will randomly select an avenger, and a villian. It will select 
-the outcome based on power and hitpoints. The Avengers winning, or the villians.
+This program has a set of 8 Avengers, the user running the program is asked to enter 8 villain
+names, and power levels. The program will randomly select an avenger, and a villain. It will select 
+the outcome based on power and hitpoints. The Avengers winning, or the villains.
 
 
 '''
 
 import random
+import pprint
 
 avengers = {
-    'Thor': {'power': 7, 'Pts': 6},
-    'Iron Man': {'power': 10, 'Pts': 10},
-    'Spiderman': {'power': 4, 'Pts': 3},
-    'Hulk': {'power': 6, 'Pts': 5},
-    'Falcon': {'power': 5, 'Pts': 4},
-    'Black Widow': {'power': 4, 'Pts': 3},
-    'Antman': {'power': 4, 'Pts': 3},
-    'Captain America': {'power': 6, 'Pts': 5}
+    'Thor': {'power': 7, 'pts': 6},
+    'Iron Man': {'power': 10, 'pts': 10},
+    'Spiderman': {'power': 4, 'pts': 3},
+    'Hulk': {'power': 6, 'pts': 5},
+    'Falcon': {'power': 5, 'pts': 4},
+    'Black Widow': {'power': 4, 'pts': 3},
+    'Antman': {'power': 4, 'pts': 3},
+    'Captain America': {'power': 6, 'pts': 5}
 }
 
 verb = [
@@ -35,22 +36,22 @@ verb = [
     'bonked'
     ]
 
-villian = {}
+villain = {}
 
 
 for i in range(8):
-    name = input('Enter villian name number ' + str(i + 1))
+    name = input('Enter villain name number ' + str(i + 1))
     try:
         power = int(input('Enter the villains power number'))
     except ValueError:
         print('Thats not a number! Please enter a number.')
 
 
-    villian[name] = {'power': power, 'Pts': 6}
+    villain[name] = {'power': power, 'pts': 6}
 
 
-print(villian)
-print(avengers)
+pprint.pprint(villain)
+pprint.pprint(avengers)
 
 
     
@@ -59,33 +60,40 @@ input('Press "Enter" to continue.')
 
 for keys in avengers:
 
-    selectedAvenger = random.choice(list(avengers.keys()))
-    selectedVillian = random.choice(list(villian.keys()))
+    selectedAvenger = {}
+    
+    random.choice(avengers.items())
+
+    print(selectedAvenger)
+
+    selectedvillain = random.choice(list(villain))
+
     selectedVerb = random.choice(verb)
+    # selects all the data used in the script ^
+
+    print(avengerData)
 
     winner = []
-    winner.append(selectedAvenger)
-    winner.append(selectedVillian)
-    
     loser = []
 
-    
+    print(winner)
+    print(loser)
 
-    print(winner[0] + ' ' + selectedVerb + ' ' +  loser)
+    #print(winner[0] + ' ' + selectedVerb + ' ' +  loser)
     
     if winner == [selectedAvenger]:
-        del villian[selectedVillian]
+        del villain[selectedvillain]
 
     else:
         del avengers[selectedAvenger]
-    # if the winner is the avengers, then the villian gets removed, and vice versa
+    # if the winner is the avengers, then the villain gets removed, and vice versa
 
-    if len(villian) == 0:
+    if villain.keys() == 0:
         break
 
 if len(avengers) >= 2:
     print('The Avengers win! The surviving members are ')
     print(avengers)
 else:
-    print('The villians win! The surviving memebrs are ')
-    print(villian)
+    print('The villains win! The surviving memebrs are ')
+    print(villain)
